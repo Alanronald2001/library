e5688f912ef0c98fd8925f3ec878e58d30b78709
alan ronald <alanronald787@gmail.com>
2025-05-08 23:32:28 +0530

    feat: added delete functionality for the library

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ const author = document.getElementById("author");
 const title = document.getElementById("title");
 const pages = document.getElementById("pages");
 const isRead = document.getElementById("isRead");
+
 const myLibrary = [
   {
     id: crypto.randomUUID(),
     author: "alan",
     title: "Live In Present",
     pages: 200,
-    isRead: false,
+    isRead: true,
   },
 ];
-
 function Book({ author, title, pages, isRead }) {
   this.id = crypto.randomUUID();
   this.author = author;
@@ -28,28 +28,47 @@ function Book({ author, title, pages, isRead }) {
 function addBookToLibrary(book) {
   const newBook = new Book(book);
   myLibrary.push(newBook);
-  loadLibrary();
+  renderNewBook(newBook);
 }
 
+function deleteFromLibrary(id) {
+  const findBook = myLibrary.findIndex((b) => b.id === id);
+  myLibrary.splice(findBook, 1);
+  loadLibrary();
+}
 function loadLibrary() {
+  library.innerHTML = "";
   myLibrary.forEach((book) => {
-    const card = document.createElement("div");
-    card.setAttribute("class", "card");
-    const title = document.createElement("div");
-    const author = document.createElement("span");
-    const pages = document.createElement("div");
-    const isRead = document.createElement("input");
-    isRead.setAttribute("type", "checkbox");
-    title.innerText = book.title;
-    card.appendChild(title);
-    author.innerText = book.author;
-    card.appendChild(author);
-    pages.innerText = book.pages;
-    card.appendChild(pages);
-    isRead.setAttribute("checked", book.isRead);
-    card.appendChild(isRead);
-    library.appendChild(card);
+    renderNewBook(book);
   });
+}
+
+function renderNewBook(book) {
+  const card = document.createElement("div");
+  card.setAttribute("class", "card");
+  const title = document.createElement("div");
+  const author = document.createElement("span");
+  const pages = document.createElement("div");
+  const isRead = document.createElement("input");
+  const delBtn = document.createElement("button");
+  isRead.setAttribute("type", "checkbox");
+  isRead.checked = book.isRead;
+  title.innerText = book.title;
+  card.appendChild(title);
+  author.innerText = book.author;
+  card.appendChild(author);
+  pages.innerText = book.pages;
+  card.appendChild(pages);
+  card.appendChild(isRead);
+  delBtn.setAttribute("class", "del-button");
+  delBtn.setAttribute("data-id", book.id);
+  delBtn.addEventListener("click", (e) => {
+    const id = delBtn.getAttribute("data-id");
+    deleteFromLibrary(id);
+  });
+  delBtn.innerText = "Delete";
+  card.appendChild(delBtn);
+  library.appendChild(card);
 }
 
 submit.addEventListener("click", (e) => {
@@ -72,3 +91,11 @@ add.addEventListener("click", () => {
   dialog.setAttribute("open", "");
 });
 loadLibrary();
+const buttons = document.querySelectorAll(".del-button");
+
+buttons.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    const id = btn.getAttribute("data-id");
+    deleteFromLibrary(id);
+  }),
+);
